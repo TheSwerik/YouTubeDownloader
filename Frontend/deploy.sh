bca14f48fab20094951202bb94fd98999622d00d
Swerik <derswerik@gmail.com>
2022-05-10 21:40:17 +0200

    add Deployment

--- Frontend/deploy.sh
+++
@@ -1,2 +0,0 @@
-﻿docker build -t frontend . --progress plain
-docker run -d -p 8080:80 frontend
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+# docker build -t YouTubeDownloader-frontend . --progress plain
+# docker compose up
+
+# Build Backend
+cd Backend || exit
+docker build -t youtubedownloader-backend . --progress plain
+cd ..
+
+# Build Frontend
+cd Frontend || exit
+docker build -t youtubedownloader-frontend . --progress plain
+cd ..
+
+# Start Compose
+docker compose -f deploy.compose.yml up -d
