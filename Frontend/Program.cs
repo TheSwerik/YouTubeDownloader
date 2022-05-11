be73cfabd5e36430e263afb0d963fbe3731c250a
Swerik <derswerik@gmail.com>
2022-05-12 00:04:33 +0200

    cleanup working

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -7,11 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
-       .AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:4200") })
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost") });
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://backend:8080") });
+       .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
        .AddScoped<DownloadService>();
 
 
