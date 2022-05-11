7cd8532ce459ddb7f06fef8b382835d8d2a14a62
Swerik <derswerik@gmail.com>
2022-05-11 23:31:51 +0200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,18 @@
 # docker compose up
 
 # Build Backend
+echo 'Building Backend'
 cd Backend || exit
 docker build -t youtubedownloader-backend . --progress plain || exit
 cd ..
 
 # Build Frontend
+echo 'Building Frontend'
 cd Frontend || exit
 docker build -t youtubedownloader-frontend . --progress plain || exit
 cd ..
 
 # Start Compose
+echo 'Starting...'
 docker compose up -d
 #docker compose -f deploy.compose.yml up -d
