32e4e1db38d3ef6f9ab3c546add60614f5738168
Swerik <derswerik@gmail.com>
2022-05-11 23:37:46 +0200

    cleanup

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -1,28 +1,19 @@
 using Backend.Service;
-using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-Console.WriteLine(builder.Configuration["ffmpegPath"]);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped(provider => new DownloadService(builder.Configuration["ffmpegPath"]));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-app.UseSwagger();
-app.UseSwaggerUI();
-// }
-
-app.UseRouting();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors(
     policyBuilder =>
@@ -35,11 +26,6 @@ app.UseCors(
                      .WithExposedHeaders("*");
     }
 );
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-                        {
-                            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-                        });
 
 app.UseAuthorization();
 
