d02ee2b2746c37f3af3decefc35fc24fc21575e6
TheSwerik <derswerik@gmail.com>
2022-05-12 17:59:01 +0200

    code cleaup

---
+++ Backend/src/Util/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using File = TagLib.File;
+
+namespace Backend.Util;
+
+public static class ExtensionMethods
+{
+    public static bool IsVideoId(this string text) { return Regex.IsMatch(text, "^[A-Za-z0-9-_]{11}$"); }
+
+    public static bool IsInvalidYouTubeUrl(this string url)
+    {
+        return !Regex.IsMatch(url, "^(https{0,1}://){0,1}(www.){0,1}(youtube.com|youtu.be).+$");
+    }
+
+    public static string AddMp3TagsToFile(this string filePath, string title, params string[] artists)
+    {
+        var tagFile = File.Create(filePath);
+        tagFile.Tag.Title = title;
+        tagFile.Tag.Performers = artists;
+        tagFile.Save();
+        return filePath;
+    }
+}
