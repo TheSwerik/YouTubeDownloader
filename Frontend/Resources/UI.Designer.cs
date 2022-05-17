0b8e90464272811c72789a7c6abcc288d23c1fff
TheSwerik <derswerik@gmail.com>
2022-05-17 09:15:22 +0200

    cleanup

--- Frontend/Resources/UI.Designer.cs
+++ Frontend/Resources/UI.Designer.cs
@@ -22,7 +22,7 @@ namespace Frontend.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class UI {
+    public class UI {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
