ae62bcc15557b9b6d318920746e5a6f952bc9860
Swerik <derswerik@gmail.com>
2024-10-23 17:50:01 +0200

    works

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -7,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
